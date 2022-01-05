4ca16b6d6fc609302dcdf2321c51579d005599bd
priyavml98 <96910236+priyavml98@users.noreply.github.com>
2022-01-05 19:00:37 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,19 @@ import { Link} from 'react-router-dom';
 import './Header.css'
 
 
-function Header() {
+function Header(props) {
+console.log(props)
+
     return (
         <div>
                
-               <nav class="navbar navbar-dark bgdark">
+  <nav class="navbar navbar-dark bgdark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="">
-      <img className='ne' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png' alt="" width="30" height="24" class="d-inline-block align-text-top"/>
-   
-    </a>
+  
+      {props.element?  props.element: 
+      <img className='ne' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png' alt="" width="100" height="24" class="d-inline-block align-text-top"/>
+      }
+    
   </div>
 </nav>
 

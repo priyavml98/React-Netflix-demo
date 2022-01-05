8cdab729e1ddff1984562dab9a847c68a1b90085
priyavml98 <96910236+priyavml98@users.noreply.github.com>
2022-01-05 19:02:10 +0530

    Update Page.js

--- src/components/Header/Page.js
+++ src/components/Header/Page.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { useLocation} from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import { imageUrl} from '../constants/Constants'
-import Hb from './Hb';
+
+import Header from './Header';
 import './Page.css'
 
 
@@ -11,13 +12,19 @@ function Page() {
   
     return (
         <div>
+            
             <br></br>
-            <Hb/>
+            
+             <Header element={obj.state.title? obj.state.title:obj.state.name}/>
              
-            <img src={`${imageUrl+obj.state.backdrop_path}`} className='star' ></img> 
+             <img src={`${imageUrl+obj.state.backdrop_path}`} className='star' /> 
             <br></br>
             <blockquote class="h">
-            <h1 className='h'> {obj.state.name? obj.state.name:obj.state.title}</h1>
+            <div>
+           <Link to={{pathname:`/`}} >  <button type="button" class="btn btn-secondary">Home</button> </Link>
+        </div>
+                
+            
             <p><em>Aired On :{obj.state.first_air_date}</em></p>
             <p><em>Rating  :{obj.state.vote_average}</em></p>
 
